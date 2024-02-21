8354d2523c37d00d49ae3f676335434d4133bb21
Trueman Phamdo <trueman@phamdo.com>
2024-02-20 18:29:44 -0800

    replace tab with space

--- nn3.py
+++ nn3.py
@@ -9,44 +9,44 @@ from torch.utils.data import Dataset
 import load_mnist
 
 def vectorized_result(j):
-	e = np.zeros((10, 1))
-	e[j] = 1.0
-	return e
+  e = np.zeros((10, 1))
+  e[j] = 1.0
+  return e
 
 class MNIST_dataset(Dataset):
-	def __init__(self, data):
-#		tr_x = np.array([np.reshape(x,(28,28)) for x in train_data[0]])
-#		tr_y = np.array([vectorized_result(y) for y in train_data[1]])
-		self.X = data[0]
-		self.Y = data[1]
-		print(self.X.shape[0])
-		print("loaded", self.X.shape, self.Y.shape)
+  def __init__(self, data):
+#    tr_x = np.array([np.reshape(x,(28,28)) for x in train_data[0]])
+#    tr_y = np.array([vectorized_result(y) for y in train_data[1]])
+    self.X = data[0]
+    self.Y = data[1]
+    print(self.X.shape[0])
+    print("loaded", self.X.shape, self.Y.shape)
 
-	def __len__(self):
-		return self.X.shape[0]
+  def __len__(self):
+    return self.X.shape[0]
 
-	def __getitem__(self, idx):
-		return (self.X[idx], self.Y[idx])
+  def __getitem__(self, idx):
+    return (self.X[idx], self.Y[idx])
 
 class Net(nn.Module):
-	def __init__(self):
-		super(Net, self).__init__()
-		self.conv1 = nn.Conv2d(1,20, kernel_size=5)
-		self.pool = nn.MaxPool2d(2, 2)
-		self.conv2 = nn.Conv2d(20,40, kernel_size=5)
-		self.fc1 = nn.Linear(40*4*4, 100)
-		self.fc2 = nn.Linear(100, 10)
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(1,20, kernel_size=5)
+    self.pool = nn.MaxPool2d(2, 2)
+    self.conv2 = nn.Conv2d(20,40, kernel_size=5)
+    self.fc1 = nn.Linear(40*4*4, 100)
+    self.fc2 = nn.Linear(100, 10)
 
-	def forward(self, x):
-		x = F.relu(self.conv1(x))
-		x = self.pool(x)
-		x = F.relu(self.conv2(x))
-		x = self.pool(x)
-		x = x.view(-1,40*4*4)
-		x = F.relu(self.fc1(x))
-		x = F.log_softmax(self.fc2(x), dim=1)
+  def forward(self, x):
+    x = F.relu(self.conv1(x))
+    x = self.pool(x)
+    x = F.relu(self.conv2(x))
+    x = self.pool(x)
+    x = x.view(-1,40*4*4)
+    x = F.relu(self.fc1(x))
+    x = F.log_softmax(self.fc2(x), dim=1)
 
-		return x
+    return x
 
 def test(test_loader, model):
     model.eval()
@@ -69,73 +69,73 @@ def test(test_loader, model):
         100. * correct / len(test_loader.dataset)))
 
 def reshape(data, is_test=False):
-	X = np.array([np.reshape(x,(28,28)) for x in data[0]])
-	Y = data[1] if is_test else np.array([vectorized_result(y) for y in data[1]])
-	return (X,Y)
+  X = np.array([np.reshape(x,(28,28)) for x in data[0]])
+  Y = data[1] if is_test else np.array([vectorized_result(y) for y in data[1]])
+  return (X,Y)
 
 if __name__ == "__main__":
-	args = {
-		'batch_size': 10
-		'test_batch_size': 10
-		'epochs': 60
-		'eta': 0.03 #learning rate
-		'cuda': False
-		'lambda': 0.1 #unused regularization param
-	}
+  args = {
+    'batch_size': 10
+    'test_batch_size': 10
+    'epochs': 60
+    'eta': 0.03 #learning rate
+    'cuda': False
+    'lambda': 0.1 #unused regularization param
+  }
 
-	'''
-	train_data, validation_data, test_data = load_mnist.load_data()
-	# reshape train/test data
-	train_data2 = reshape(train_data)
-	test_data2 = reshape(test_data, is_test=True)
+  '''
+  train_data, validation_data, test_data = load_mnist.load_data()
+  # reshape train/test data
+  train_data2 = reshape(train_data)
+  test_data2 = reshape(test_data, is_test=True)
 
-	mnist_train_data = MNIST_dataset(train_data2)
-	mnist_test_data = MNIST_dataset(test_data2)
-	training_loader=torch.utils.data.DataLoader(mnist_train_data, batch_size=args['batch_size'], shuffle=True)
-	test_loader=torch.utils.data.DataLoader(mnist_test_data, batch_size=args['test_batch_size'], shuffle=True)
-	'''
-	train_loader = torch.utils.data.DataLoader(
-			datasets.MNIST('./data', train=True, download=True,
-									transform=transforms.Compose([
+  mnist_train_data = MNIST_dataset(train_data2)
+  mnist_test_data = MNIST_dataset(test_data2)
+  training_loader=torch.utils.data.DataLoader(mnist_train_data, batch_size=args['batch_size'], shuffle=True)
+  test_loader=torch.utils.data.DataLoader(mnist_test_data, batch_size=args['test_batch_size'], shuffle=True)
+  '''
+  train_loader = torch.utils.data.DataLoader(
+      datasets.MNIST('./data', train=True, download=True,
+                  transform=transforms.Compose([
                        transforms.ToTensor(),
-											 transforms.Normalize((0.1307,), (0.3081,))
+                       transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=args['batch_size'], shuffle=True)
-	test_loader = torch.utils.data.DataLoader(
-			datasets.MNIST('./data', train=False, transform=transforms.Compose([
+  test_loader = torch.utils.data.DataLoader(
+      datasets.MNIST('./data', train=False, transform=transforms.Compose([
                        transforms.ToTensor() ,
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=args['test_batch_size'], shuffle=True)
 
-	model = Net()
-	criterion = nn.NLLLoss()
-	optimizer = optim.SGD(model.parameters(), lr=args['eta'])
+  model = Net()
+  criterion = nn.NLLLoss()
+  optimizer = optim.SGD(model.parameters(), lr=args['eta'])
 
-	if args['cuda']:
-		model.cuda()
+  if args['cuda']:
+    model.cuda()
 
 
-	test(test_loader, model)
-	print(test_loader.dataset.shape)
-	for epoch in range(args['epochs']):
-		model.train()
-		running_loss = 0.0
-		for batch_idx, (data, target) in enumerate(train_loader):
-			if (args['cuda']):
-				data, target = data.cuda(), target.cuda()
+  test(test_loader, model)
+  print(test_loader.dataset.shape)
+  for epoch in range(args['epochs']):
+    model.train()
+    running_loss = 0.0
+    for batch_idx, (data, target) in enumerate(train_loader):
+      if (args['cuda']):
+        data, target = data.cuda(), target.cuda()
 
-			optimizer.zero_grad()
+      optimizer.zero_grad()
 
-			output = model(data)
-			loss = criterion(output, target)
-			loss.backward()
-			optimizer.step()
+      output = model(data)
+      loss = criterion(output, target)
+      loss.backward()
+      optimizer.step()
 
-			# print statistics
-			running_loss += loss.item()
-			if batch_idx % 2000 == 1999:    # print every 2000 mini-batches
-				print(f'[Epoch: {epoch + 1}, Examples: {(batch_idx+1) * args["batch_size"]}] loss: {running_loss / 2000:.4f}')
-				running_loss = 0.0
+      # print statistics
+      running_loss += loss.item()
+      if batch_idx % 2000 == 1999:    # print every 2000 mini-batches
+        print(f'[Epoch: {epoch + 1}, Examples: {(batch_idx+1) * args["batch_size"]}] loss: {running_loss / 2000:.4f}')
+        running_loss = 0.0
 
-		test(test_loader, model)
+    test(test_loader, model)
